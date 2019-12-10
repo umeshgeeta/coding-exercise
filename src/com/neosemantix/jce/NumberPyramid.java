5aac26d20cae15b3866bf0e21803f51858a8cb28
umeshpatil <umesh409@yahoo.com>
2019-12-09 19:31:38 -0800

    Committing some left over changes.

--- src/com/neosemantix/jce/NumberPyramid.java
+++ src/com/neosemantix/jce/NumberPyramid.java
@@ -1,6 +1,5 @@
 package com.neosemantix.jce;
 
-import java.util.List;
 import java.util.Random;
 
 /**
--- src/com/neosemantix/jce/StringExercise.java
+++ src/com/neosemantix/jce/StringExercise.java
@@ -1,5 +1,7 @@
 package com.neosemantix.jce;
 
+import java.util.Arrays;
+
 public class StringExercise {
 	
 	public static String reverse(String arg) {
@@ -22,13 +24,45 @@ public class StringExercise {
 		}
 		return result;
 	}
+	
+	public static boolean isAnagram(String str1, String str2) {
+		boolean result = false;
+		if (str1 != null) {
+			if (str2 != null) {
+				String s1 = str1.replaceAll("\\s", "");
+				String s2 = str2.replaceAll("\\s", "");
+				int l1 = s1.length();
+				int l2 = s2.length();
+				if (l1 == l2) {
+					char[] c1 = s1.toLowerCase().toCharArray();
+					char[] c2 = s2.toLowerCase().toCharArray();
+					Arrays.sort(c1);
+					Arrays.sort(c2);
+//					System.out.println(c1);
+//					System.out.println(c2);
+					result = Arrays.equals(c1, c2);
+				}
+				// lengths are unequal, they are not anagram
+			}
+			// first is not null, while second is not; they are not anagram
+		} else if (str2 == null) {
+			// both are null, we regard them anagram trivially
+			result = true;
+		}
+		// else first is null, second is not; they are not anagram
+		return result;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String str1 = "test";
-		String str2 = "house";
-		System.out.println(reverse(str1));
-		System.out.println(reverse(str2));
+//		String str1 = "test";
+//		String str2 = "house";
+//		System.out.println(reverse(str1));
+//		System.out.println(reverse(str2));
+		
+		String s1 = "Mother In Law";
+		String s2 = "Hitler Woman";
+		System.out.println(s1 + " and " + s2 + " are Anagrams? " + isAnagram(s1, s2));
+		
 	}
 
 }
